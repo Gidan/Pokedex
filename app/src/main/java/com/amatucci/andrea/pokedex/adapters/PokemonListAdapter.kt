90e5dfd7ec4db9eac6d45a72bf80c3a70a7c8f94
Andrea Amatucci <amatucci.andrea@gmail.com>
2021-04-12 11:51:14 +0200

    pokemon image in list now fades in

--- app/src/main/java/com/amatucci/andrea/pokedex/adapters/PokemonListAdapter.kt
+++ app/src/main/java/com/amatucci/andrea/pokedex/adapters/PokemonListAdapter.kt
@@ -16,13 +16,18 @@ import com.amatucci.andrea.pokedex.util.PokemonDataUtil
 import com.amatucci.andrea.pokedex.util.blendColors
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import java.util.*
 
+
 interface OnItemClickListener {
-    fun onItemClicked(position : Int, commonView : View, pokemon: Pokemon?)
+    fun onItemClicked(position: Int, commonView: View, pokemon: Pokemon?)
 }
 
-class PokemonListAdapter(private val onItemClickListener: OnItemClickListener) : PagingDataAdapter<Pokemon, RecyclerView.ViewHolder>(PokemonDiffCallback()) {
+class PokemonListAdapter(private val onItemClickListener: OnItemClickListener) : PagingDataAdapter<Pokemon, RecyclerView.ViewHolder>(
+    PokemonDiffCallback()
+) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return PokemonViewHolder(
@@ -39,14 +44,25 @@ class PokemonListAdapter(private val onItemClickListener: OnItemClickListener) :
         (holder as PokemonViewHolder).bind(pokemon)
     }
 
-    class PokemonViewHolder(private val binding: ListItemPokemonBinding, private val onItemClickListener: OnItemClickListener) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+    class PokemonViewHolder(
+        private val binding: ListItemPokemonBinding,
+        private val onItemClickListener: OnItemClickListener
+    ) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
         private var pokemon: Pokemon? = null
+        companion object {
+            val transitionFactory: DrawableCrossFadeFactory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
+        }
 
         fun bind(pokemon: Pokemon?){
             this.pokemon = pokemon
             binding.apply {
-                pokemonBlendColor.setBackgroundColor(ContextCompat.getColor(root.context, android.R.color.transparent))
+                pokemonBlendColor.setBackgroundColor(
+                    ContextCompat.getColor(
+                        root.context,
+                        android.R.color.transparent
+                    )
+                )
                 pokemon?.let {
                     txtPokemonName.text = pokemon.name.toUpperCase(Locale.getDefault())
                     txtNum.text = root.context.getString(R.string.pokemon_item_number, pokemon.id)
@@ -55,11 +71,15 @@ class PokemonListAdapter(private val onItemClickListener: OnItemClickListener) :
                     Glide.with(root)
                         .load(imgUrl)
                         .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .placeholder(R.drawable.ic_pokeball)
+                        .transition(DrawableTransitionOptions.withCrossFade())
                         .into(ivPokemonArtwork)
                     type1.setType(pokemon.types[0].type.name)
                     type2.setType(if (pokemon.types.size > 1) pokemon.types[1].type.name else null)
-                    val map = pokemon.types.map { it.type.name }.map { ContextCompat.getColor(root.context, Type.valueOf(it).colorRes) }
+                    val map = pokemon.types.map { it.type.name }.map { ContextCompat.getColor(
+                        root.context, Type.valueOf(
+                            it
+                        ).colorRes
+                    ) }
                     val blendColor = blendColors(*map.toIntArray())
 
                     bgBlendColor.alpha = 0.0f
